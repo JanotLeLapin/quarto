b8aeaf440d5b135bee8496cf6630c4b50eae09ac
Joseph DALY <joseph300905@gmail.com>
2026-02-13 22:59:07 +0100

    core: test board bounds

--- core/src/board.rs
+++ core/src/board.rs
@@ -8,10 +8,18 @@ impl Board {
     }
 
     pub fn get_piece(&self, x: usize, y: usize) -> Option<Piece> {
+        if x >= 4 || y >= 4 {
+            return None;
+        }
+
         self.0[x][y]
     }
 
     pub fn set_piece(&mut self, x: usize, y: usize, piece: Option<Piece>) {
+        if x >= 4 || y >= 4 {
+            return;
+        }
+
         self.0[x][y] = piece;
     }
 }
